b5f557838db85deeeab48b3a690ebe4ed2617fc3
christine <christine.muriungi@student.moringaschool.com>
2023-10-12 12:18:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(a="*") {
+    return function(parameter="special") {
+        return `You are ${a}${parameter}${a}!`
+    }
+}
